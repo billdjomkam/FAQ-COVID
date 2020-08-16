b16bd785f051a4a1ded8051ba9312cdeb9681785
billdjomkam <billdjomkam99@gmail.com>
2020-08-16 19:52:43 +0200

    version disigner

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+$DB['server'] = '127.0.0.1';
+$DB['port'] = '5432';
+$DB['user'] = 'postgres';
+$DB['password'] = 'maurice1999';
+$DB['db'] = 'FAQCOVID19';
+$DB['prefix'] = '';
+$DB['type'] = 'pgsql';
